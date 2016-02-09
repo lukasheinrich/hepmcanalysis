61f1018600b7ff5a602a4293c8d55938afb18422
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-02-09 10:57:05 -0500

    add dumper

--- hepmcanalysis/events.py
+++ hepmcanalysis/events.py
@@ -1,6 +1,17 @@
+import hepmc
+from hepmcanalysis.streamproxy import ostringstream_proxy
+
 def events(genevent):
   '''simple generator that exhausts the read_next_event of a GenEvent object'''
   ev = genevent.read_next_event()
   while ev is not None:
      yield ev
      ev = genevent.read_next_event()
+
+def dumps(event):
+    outproxy = ostringstream_proxy()
+    outevent = hepmc.IO_GenEvent(outproxy.stream())
+    outevent.write_event(event)
+    del outevent
+    output =  outproxy.str()
+    return '\n'.join(output.split('\n')[3:-2])    
